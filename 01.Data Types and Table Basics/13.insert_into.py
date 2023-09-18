7e560902ae83510e71fdc54c7801ab8f524c24a4
Mihail Mihov <130042610+mihov97@users.noreply.github.com>
2023-09-18 15:30:19 +0300

    Update 13.insert_into.py

--- 01.Data Types and Table Basics/13.insert_into.py
+++ 01.Data Types and Table Basics/13.insert_into.py
@@ -1,6 +1,6 @@
-INSERT INTO minions_info
+INSERT INTO minions_info 
 	(name, code, task, banana, email, equipped, mood)
-VALUES
+VALUES 
 	('Mark', 'GKYA', 'Graphing Points', 3265.265, 'mark@minion.com', false, 'happy'),
 	('Mel', 'HSK', 'Science Investigation', 54784.996, 'mel@minion.com', true, 'stressed'),
 	('Bob', 'HF', 'Painting', 35.652, 'bob@minion.com', true, 'happy'),
